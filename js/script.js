e25ebabfef9eeba1665a22cc8c799e62fcc53099
shraboneeakther <107603396+shraboneeakther@users.noreply.github.com>
2022-06-30 00:01:11 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$('.one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    
+    responsive:{
+        0:{
+            items:2
+        },
+        425:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        1024:{
+            items:5
+        }
+    }
+})
+
+$('.two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        425:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        1024:{
+            items:5
+        }
+    }
+})
